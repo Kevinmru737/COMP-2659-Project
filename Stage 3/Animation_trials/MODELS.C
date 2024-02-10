73ab45c9ae78834abf4a634bced8861a8707431e
Kevinmru737 <78282516+Kevinmru737@users.noreply.github.com>
2024-02-10 16:14:54 -0700

    Add files via upload

---
+++ Stage 3/Animation_trials/MODELS.C
@@ -0,0 +1,63 @@
+#include "models.h"
+
+Player* create_player(int x, int y) {
+	Player* new_player;
+	new_player->top_left.x_pos = x;
+	new_player->top_left.y_pos = y;
+	new_player->top_right.x_pos = x + 31;
+	new_player->top_right.y_pos = y;
+	new_player->bot_left.x_pos = x;
+	new_player->bot_left.y_pos = y - 31;
+	new_player->bot_right.x_pos = x + 31;
+	new_player->bot_right.y_pos = y - 31;
+
+	new_player->vert_vel = 0;
+	new_player->is_jumping = FALSE;
+	return new_player;
+}
+void jump(Player * player) {
+
+	player->is_jumping = TRUE;
+	player->vert_vel = -10;
+
+}
+/* Moves the player in the vertical direction by updating its y position
+   with its vertical velocity
+*/
+void move_player(Player * player) {
+	if (player->is_jumping == TRUE) {
+		player->top_left.y_pos += (player->vert_vel);
+		player->top_right.y_pos += (player->vert_vel);
+		player->bot_left.y_pos += (player->vert_vel);
+		player->bot_right.y_pos += (player->vert_vel);
+		(player->vert_vel)++;
+	}
+}
+
+/* Sets the jump property to false to stop the player's jump sequence
+*/
+void stop_jump(Player * player) {
+
+	player->is_jumping = FALSE;
+	player->vert_vel = 0;
+}
+/* Moves the triangle in the horizontal left direction
+*/
+void move_triangle_obs(Triangle* triangle) {
+
+	triangle->bot_left.x_pos += triangle->hor_vel;
+	triangle->top.x_pos += triangle->hor_vel;
+	triangle->bot_right.x_pos += triangle->hor_vel;
+}
+
+
+
+/* Moves the platform in the horizontal left direction
+*/
+void move_platform_(Platform* platform) {
+
+	platform->top_left.x_pos += platform->hor_vel;
+	platform->top_right.x_pos += platform->hor_vel;
+
+};
+
