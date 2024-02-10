82f0594518d2fafed10eff1925a9bf58df780dae
Kevinmru737 <78282516+Kevinmru737@users.noreply.github.com>
2024-02-10 14:36:48 -0700

    Add files via upload

---
+++ Stage 3/events.c
@@ -0,0 +1,22 @@
+#include "types.h"
+#include "models.h"
+
+void spawn_triangle(int x, int y) {
+
+}
+
+void spawn_platform(int x, int y) {
+
+}
+
+void player_death() {
+
+}
+
+void player_victory() {
+
+}
+
+void player_jumping(Player* player) {
+
+}
