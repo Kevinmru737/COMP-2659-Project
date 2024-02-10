cfb2c0d72bc5ca62f2c41a3e852e7fc0d8e408c3
Depanshu Suyal <122829642+Suya678@users.noreply.github.com>
2024-02-10 13:03:37 -0700

    Add files via upload

---
+++ Stage 3/MODELS.C
@@ -0,0 +1,48 @@
+#include "models.h"
+
+/*
+	Initiates the jump sequence fo the player
+*/
+void jump(Player * player) {
+
+	player->is_jumping = TRUE;
+	player->vert_vel = 5;
+
+}
+/* Moves the player in the vertical direction by updating its y position
+   with its vertical velocity
+*/
+void move_player(Player * player) {
+	player->top_left.y_pos += (player->vert_vel);
+	player->top_right.y_pos += (player->vert_vel);
+	player->bot_left.y_pos += (player->vert_vel);
+	player->bot_right.y_pos += (player->vert_vel);
+	(player->vert_vel)--;
+}
+
+/* Sets the jump property to false to stop the player's jump sequence
+*/
+void stop_jump(Player * player) {
+
+	player->is_jumping = FALSE;
+	player->vert_vel = 0;
+}
+/* Moves the triangle in the horizontal left direction
+*/
+void move_triangle_obs(Triangle* triangle) {
+
+	triangle->bot_left.x_pos += triangle->hor_vel;
+	triangle->top.x_pos += triangle->hor_vel;
+	triangle->bot_right.x_pos += triangle->hor_vel;
+}
+
+
+
+/* Moves the platform in the horizontal left direction
+*/
+void move_platform_(Platform* platform) {
+
+	platform->top_left.x_pos += platform->hor_vel;
+	platform->top_right.x_pos += platform->hor_vel;
+
+};
