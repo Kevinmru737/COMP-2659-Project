9abc581ec58c0d6315701a8a17f7808f3e01bec8
Kevinmru737 <78282516+Kevinmru737@users.noreply.github.com>
2024-02-12 15:11:06 -0700

    Add files via upload

--- Stage 3/events.c
+++ Stage 3/events.c
@@ -17,13 +17,20 @@ void player_death() {
 void player_victory() {
 
 }
-
+/* Author: Kevin
+ * Purpose: This event occurs when a player jump should occur and 
+ *          continues until the player is no longer jumping.
+ *
+ * Details:
+ *   Calls the function to move the player.
+ *
+ * Restriction:
+ *  Not called directly by user code; used internally by draw_32_bit_bitmap.
+ */
 void player_jumping(Player* player) {
-    jump(player);
-    while (player->top_left.y_pos > 250) {
+    if (player->is_jumping == TRUE) {
         move_player(player);
-    }
-    stop_jump(player);
+    } 
 }
 
 
