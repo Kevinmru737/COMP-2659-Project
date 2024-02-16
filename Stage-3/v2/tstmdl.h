e7971fff56a083788568149c3bbf44d1b9665a1b
Depanshu Suyal <122829642+Suya678@users.noreply.github.com>
2024-02-15 18:55:43 -0700

    Add files via upload

---
+++ Stage-3/v2/tstmdl.h
@@ -0,0 +1,24 @@
+#ifndef TST_MODEL_H
+#define TST_MODEL_H
+
+#include <stdio.h>
+#include "models.h"
+#include "events.h"
+#include "osbind.h"
+#include <unistd.h>
+
+
+void print_full_triangle_details(Triangle* test_triangle);
+void print_full_player_details(Player* test_player);
+void print_ground(Ground* test_ground);
+void print_full_platform_details(Platform* test_platform);
+
+void test_platform_initializer();
+void test_triangle_initializer();
+void test_player_initializer();
+void wait_for_user();
+
+void test_player_jump();
+void test_player_move();
+
+#endif
