3e975e14651c4a719fe8b692eb626b57319a68c7
Depanshu Suyal <122829642+Suya678@users.noreply.github.com>
2024-02-23 10:08:17 -0700

    Add files via upload
    Changed the ascending and descending acceleration. Made the collision checking more. robust

---
+++ stage-5/input.h
@@ -0,0 +1,13 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+
+
+#include <osbind.h>
+
+#include "bool.h"
+
+/*changes the inp value if a valid key (esc or space) was pressed*/
+void user_input(char *inp);
+
+#endif
