df4a4a62efad428eab661437489503c898afc85f
Depanshu Suyal <122829642+Suya678@users.noreply.github.com>
2024-03-25 21:43:28 -0600

    Add files via upload

---
+++ stage-9b/kbd.h
@@ -0,0 +1,42 @@
+#ifndef kbd_h
+#define kbd_h
+#include "vct_inst.h"
+#include <osbind.h>
+#include "macros.h"
+
+
+typedef unsigned char SCANCODE;
+
+
+#define KBD_STATE_NO_MOUSE 0
+#define KBD_STATE_2nd_MOUSE 1
+#define KBD_STATE_3rd_MOUSE 2
+
+#define KBD_VEC_NUM 70
+#define KBD_BUFFER_SIZE 64
+#define BUFFER_EMPTY_VALUE -1
+
+
+
+
+void restore_keyboard_isr();
+void init_keyboard_isr();
+void kbd_isr();
+unsigned char get_value_from_buffer();
+bool is_buffer_empty();
+
+void put_value_in_buffer(unsigned char value);
+void kbd_driver();
+
+
+typedef struct {
+    int front;
+   int rear;
+   unsigned char buffer[KBD_BUFFER_SIZE];
+ 
+}Kbd_Buffer;
+
+
+
+
+#endif
---
+++ stage-9b/kbd_asm.s
@@ -0,0 +1,8 @@
+    xref _kbd_driver
+    xdef _kbd_isr
+
+_kbd_isr: 	movem.l	d0-3/a0-3,-(sp)
+	jsr     _kbd_driver
+	movem.l (sp)+,d0-3/a0-3
+	move.b   #$BF,$FFFA11
+	rte
