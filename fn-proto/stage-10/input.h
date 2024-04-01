9a884df1367df639a92755403c0414b0ecc7509c
Depanshu Suyal <122829642+Suya678@users.noreply.github.com>
2024-03-31 19:09:51 -0600

    Add files via upload
    Final prototype

---
+++ fn-proto/stage-10/input.h
@@ -0,0 +1,36 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+
+
+#include "macros.h"
+#include "kbd.h"
+#define MOUSE_LEFT_CLICK_VALUE      0xFA
+#define MOUSE_RIGHT_CLICK_VALUE      0xF9
+#define ALL_BUTTONS_MAX_Y           380
+#define ALL_BUTTONS_MIN_Y           355
+
+
+
+#define ONE_PLAYER_BUTTON_MIN_X          115
+#define ONE_PLAYER_BUTTON_MAX_X          180
+
+#define PAUL_MODE_BUTTON_MIN_X          340
+#define PAUL_MODE_BUTTON_MAX_X           405
+
+
+#define EXIT_BUTTON_MIN_X          450
+#define EXIT_BUTTON_MAX_X           515
+
+/*changes the inp value if a valid key (esc or space) was pressed*/
+bool user_pressed_space();
+UINT8 get_mouse_button_status();
+typedef enum {
+    NO_SELECTION,
+    SELECTED_PAUL_MODE,
+    SELECTED_1_PLAYER,
+    SELECTED_EXIT
+}CURSOR_STATUS;
+
+CURSOR_STATUS  user_mouse_input();
+#endif
